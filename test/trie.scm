f6382e5f2b0e9b0879ef5b0149a74eb783357c97
aharisu <foo.yobina@gmail.com>
2014-06-27 22:15:28 +0900

    Added trie-common-refix-continuation function into util.trie module.

---
+++ test/trie.scm
@@ -0,0 +1,70 @@
+(use gauche.test)
+(test-start "extend-trie")
+
+(load "trie.scm")
+
+(use gauche.sequence)
+(use srfi-1)
+
+(let* ([keys '("hoge" "hoga" "hi" "hikari")]
+       [trie-test
+         (lambda (t)
+           (for-each-with-index 
+             (lambda (index key) (trie-put! t key index))
+             keys)
+           (test* "prefix continuation" #t
+             (is-a? (trie-common-prefix-continuation t #\h) <trie>))
+           (test* "prefix continuation" #t
+             (is-a? 
+               (trie-common-prefix-continuation
+                 (trie-common-prefix-continuation t #\h)
+                 #\o)
+               <trie>))
+           (test* "prefix continuation" #t
+             (is-a? 
+               (trie-common-prefix-continuation
+                 (trie-common-prefix-continuation
+                   (trie-common-prefix-continuation t #\h)
+                   #\o)
+                 #\g)
+               <trie>))
+           (test* "prefix continuation" (cons #t 0)
+             (receive (cont v)
+               (car+cdr
+                 (trie-common-prefix-continuation
+                   (trie-common-prefix-continuation
+                     (trie-common-prefix-continuation
+                       (trie-common-prefix-continuation t #\h)
+                       #\o)
+                     #\g)
+                   #\e))
+               (cons (is-a? cont <trie>) v)))
+           (test* "prefix continuation" #f
+             (trie-common-prefix-continuation
+               (car
+                 (trie-common-prefix-continuation
+                   (trie-common-prefix-continuation
+                     (trie-common-prefix-continuation
+                       (trie-common-prefix-continuation t #\h)
+                       #\o)
+                     #\g)
+                   #\e))
+               #\a))
+           (test* "prefix continuation" #f
+             (trie-common-prefix-continuation t #\a))
+           )])
+  (trie-test (make-trie))
+  (trie-test (make-trie
+               list
+               (cut assoc-ref <> <> #f char-ci=?)
+               (lambda (t k v)
+                 (if v
+                   (assoc-set! t k v char-ci=?)
+                   (alist-delete! k t char-ci=?)))
+               (lambda (t f s) 
+                 (fold (lambda (node acc) (f (car node) (cdr node) acc)) s t))
+               ))
+  )
+
+
+(test-end)
---
+++ trie.scm
@@ -0,0 +1,36 @@
+(use util.trie)
+
+(select-module util.trie)
+
+(export trie-common-prefix-continuation)
+
+(define (%trie-get-node-from-elt trie elt)
+  (and-let* ([table (%node-table (slot-ref trie 'root))])
+    ((slot-ref trie 'tab-get)  table elt)))
+
+(define (%trie-prefix-elt-collect trie prefix-elt getter)
+  (or (and-let* ([node (%trie-get-node-from-elt trie prefix-elt )])
+        (let ([sub-trie (make <trie>
+                              :tab-make (slot-ref trie 'tab-make)
+                              :tab-get (slot-ref trie 'tab-get)
+                              :tab-put! (slot-ref trie 'tab-put!)
+                              :tab-fold (slot-ref trie 'tab-fold))]
+              [terminal (%node-terminals node)])
+          (slot-set! sub-trie 'root node)
+          (if (null? terminal)
+            sub-trie
+            (cons sub-trie (cdar terminal)))))
+    #f))
+
+(define (trie-common-prefix-continuation trie prefix-elt)
+  (%trie-prefix-elt-collect trie prefix-elt (^ [k v] (cons k v))))
+
+(define t (make-trie list
+                     (cut assoc-ref <> <> #f char-ci=?)
+                     (lambda (t k v)
+                       (if v
+                         (assoc-set! t k v char-ci=?)
+                         (alist-delete! k t char-ci=?)))
+                     (lambda (t f s) 
+                       (fold (lambda (node acc) (f (car node) (cdr node) acc)) s t))))
+
